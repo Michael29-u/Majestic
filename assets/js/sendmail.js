2eedf5973b523bfc817be7ddf8fcd70667811d3d
Majestic <Majestic@gmail.com>
2025-08-28 18:00:46 +0100

    add js

---
+++ assets/js/sendmail.js
@@ -0,0 +1,12 @@
+function sendMail() {
+   let params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      subject: document.getElementById("subject").value,
+      message: document.getElementById("message").value,
+  
+   } 
+
+    emailjs.send("service_ogljbl6","template_9aukr5p",params).then(alert("Message sent successfully!!"))
+    
+  } 
