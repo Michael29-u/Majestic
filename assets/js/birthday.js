a945c656e4463dd2585d6bdc19d53e0bfddf3822
maj <maj@gmail.com>
2026-02-11 11:28:13 +0100

    add image

--- assets/js/birthday.js
+++ assets/js/birthday.js
@@ -15,7 +15,7 @@ const list = document.getElementById("messages-list");
 const countdownEl = document.getElementById("birthday-countdown");
 
 // 🎂 Your birthday (MM-DD)
-const BIRTHDAY = "02-11";
+const BIRTHDAY = "08-29";
 
 // === Utility Functions ===
 
